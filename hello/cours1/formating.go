93ef1abf6a564146cf1371824ffd27e974a398ba
xizhe2 <xiaoxia.zheng.1@ulaval.ca>
2017-04-23 19:38:38 -0400

    remove two files

--- hello/cours1/formating.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Printf("%d %b", 5, 5)
-	fmt.Printf("%d %b", 5, 5)
-}
--- hello/cours1/loop.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Printf("%d %b", 5, 5)
-}
